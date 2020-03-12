59c4e67b8d1c1b67db0402fcaa1a8c21c6595a4a
Ricardo Ruiz Giler <ricardo@Ricardos-MacBook-Pro.local>
2020-03-12 22:56:42 +0300

    Updates

---
+++ precache-manifest.5cb6cc6b82bd87ab91bf1ccb19d33683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec22ebecad3410156184511d6cb98cb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "18cf3632e62422646c65",
+    "url": "/monsters-rolodex/static/css/main.8a46b385.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18cf3632e62422646c65",
+    "url": "/monsters-rolodex/static/js/main.caa7b666.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
